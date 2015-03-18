b5f6b21563038c8db2700d0d03c1bea8244f0192
Shrayas <shrayasr@gmail.com>
2015-03-18 23:02:36 +0530

    You can pass variables to the templates too!
    Here, we are passing the current date and time to the template. Inside
    the template you can use `{{` and `}}` in order to refer to a variable
    that is being sent from the python program

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", now=str(datetime.now()))
 
 if __name__ == "__main__":
     app.run(debug=True)
