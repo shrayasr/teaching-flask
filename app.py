efa4270bb0a93ce9d6dad59ee070d96284375cd8
Shrayas <shrayasr@gmail.com>
2015-03-18 23:07:46 +0530

    More complex template rendering
    Templates can use statements like `for` inside them as well. This part
    takes a simple list that was defined and passes it to the template. The
    template then iterates over the passes list and puts each one of them
    as a list item within a unordered list. Now navigating to
    `http://localhost:5000/customers` should get you this page that we just
    made

--- app.py
+++ app.py
@@ -3,9 +3,18 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+list_of_customers = [
+        "Bharati Bhawan",
+        "Ramakrishna Mutt"
+        ]
+
 @app.route("/")
 def index():
     return render_template("index.html", now=str(datetime.now()))
+
+@app.route("/customers/")
+def customers():
+    return render_template("customers.html", customers=list_of_customers)
 
 if __name__ == "__main__":
     app.run(debug=True)
